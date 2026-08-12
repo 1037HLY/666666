ddb757564364f1c2571c202383623464faf0e1ec
1037HLY <hs202027323@gmail.com>
2026-08-12 11:20:27 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("kotlin-kapt") version "1.9.20" apply false
-    id("kotlin-parcelize") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.20" apply false
 }
 
 tasks.register("clean", Delete::class) {

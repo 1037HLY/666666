82673f4cc67eacfa9a01783325020e4472be8056
1037HLY <hs202027323@gmail.com>
2026-08-12 12:14:05 +0800

    Create Converters.kt

---
+++ app/src/main/java/com/geosurvey/toolbox/data/database/Converters.kt
@@ -0,0 +1,19 @@
+package com.geosurvey.toolbox.data.database
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+/**
+ * Room类型转换器
+ */
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
